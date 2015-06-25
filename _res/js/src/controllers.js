7e87ab7f163abfd23eacd02a0f17c011828d0797
TitoAgudelo <titoarturoagudelo@gmail.com>
2015-06-25 08:33:38 -0500

    angular tabs implementation

--- _res/js/src/controllers.js
+++ _res/js/src/controllers.js
@@ -12,14 +12,20 @@ ctrls.controller('WidgetsCtrl', ['$scope', '$routeParams',
 
 ctrls.controller('GraphCtrl', ['$scope', '$routeParams', '$location',
 	function($scope, $routeParams, $location) {
+		
+		$scope.viewTab =  true;
+    	$scope.tab = true;
+
+
 		$scope.isActive = function (viewLocation) { 
-	        if ("/diagrama" == viewLocation){
-	        	$scope.viewTab = false;
-	        } else if("/mount" == viewLocation) {
+	        if ("#diagrama" == viewLocation){
 	        	$scope.viewTab = true;
+	        	$scope.tab = true;
+	        } else if("#month" == viewLocation) {
+	        	$scope.viewTab = false;
+	        	$scope.tab = false;
 	        }
 	        return viewLocation === $location.url();
-
 	    };
 	}
 ]);
