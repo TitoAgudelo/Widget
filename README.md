b4807ae1c8d2a0dda2b1a9464dd56c56d6685b57
TitoAgudelo <titoarturoagudelo@gmail.com>
2015-06-26 16:12:57 -0500

    last one widget

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # Widget
 Test for create a visual widgets with Bower, AngularJS, Compass, Grunt, Foundation Zurb for responsive and reveal scroll for do a some animation with scroll 
+
+This README outlines the details of collaborating on this Angular application.
+
+## Prerequisites
+
+You will need the following things properly installed on your computer.
+
+* [Git](http://git-scm.com/)
+* [Node.js](http://nodejs.org/) (with NPM)
+* [Bower](http://bower.io/)
+* [Compass](http://compass-style.org/)
+* [Foundation Zurb](http://foundation.zurb.com/)
+* [Highcharts](http://www.highcharts.com/)
+* [Grunt](http://gruntjs.com/)
+* [AngularJS](https://angularjs.org/)
+
+## Installation
+
+* `git clone git@github.com:TitoAgudelo/Widget.git`
+* `cd Widget`
+* `npm install`
+* `bower install`
+* `compass watch _res`
+* `cd _res`
+* `grunt`
+
+## Running / Development
+
+* `python -m SimpleHTTPServer 8080`
+* Visit your app at [http://localhost:8080](http://localhost:8080).
+
+### Code Generators
+
+Make use of the many generators for code, try `ember help generate` for more details
+
+### Building
+
+* `compass building` (development)
+* `grunt building` (development)
+
+### Deploying
+
+local
+
+### Author
+Tito Agudelo
--- _res/js/src/app.js
+++ _res/js/src/app.js
@@ -24,10 +24,32 @@ app.config(["$routeProvider", "$locationProvider", function ($routeProvider, $lo
 
 app.run(['$rootScope', '$http',
 	function($rootScope, $http) {
-			var jsonListMenu = 'stubs/menu.json',
+			
+			$rootScope.changeLanguage = function (lan){
+				$rootScope.langu = lan;
+				app.run();
+			};
+
+			if ($rootScope.langu == "es") {
+				// json english and	default	
+				var jsonListMenu = 'stubs/menues.json',
+				jsonProfile = 'stubs/profilees.json',
+				jsonLocation = 'stubs/locationes.json',
+				jsonData = 'stubs/dataTransferes.json',
+				jsonCategory = 'stubs/categoryes.json',
+				jsonDropdown = 'stubs/dropdown.json';
+				$rootScope.languages = true;
+			} else {
+				// json english and	default	
+				var jsonListMenu = 'stubs/menu.json',
 				jsonProfile = 'stubs/profile.json',
 				jsonLocation = 'stubs/location.json',
-				jsonData = 'stubs/dataTransfer.json';
+				jsonData = 'stubs/dataTransfer.json',
+				jsonCategory = 'stubs/category.json',
+				jsonDropdown = 'stubs/dropdown.json';
+				$rootScope.languages = false;
+			}
+			
 
 			$http.get(jsonListMenu).success(function(data){
 				$rootScope.items = data;
@@ -44,5 +66,15 @@ app.run(['$rootScope', '$http',
 			$http.get(jsonData).success(function(data){
 				$rootScope.diagrams = data;
 			});
+
+			$http.get(jsonCategory).success(function(data){
+				$rootScope.categories = data;
+			});
+
+			$http.get(jsonDropdown).success(function(data){
+				$rootScope.dropdowns = data;
+			});
+
+			
 		}
 	]);
